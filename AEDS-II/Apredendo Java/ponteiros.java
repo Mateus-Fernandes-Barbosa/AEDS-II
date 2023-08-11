c5e97ea803871cce6bc068a8c4ff257e64abf6bf
Mateus Fernandes Barbosa <mateusfb04@gmail.com>
2023-08-11 06:48:27 -0300

    Create ponteiros.java

---
+++ AEDS-II/Apredendo Java/ponteiros.java
@@ -0,0 +1,24 @@
+class ponteiro{
+	public static void passagemDeArray(int [] b){
+		for(int i = 0; i<5; i++){
+			b[i] *= 5;
+			System.out.println("b["+i+"]:"+b[i]);
+		}
+		b = new int[5];
+		for(int i = 0; i < 5; i++){
+			b[i] = i;
+			System.out.println("b["+i+"]:"+b[i]);
+		}
+	}
+	public static void main(String [] args){
+	       int [] y = new int[5];
+       		for(int i = 0; i < 5; i++){
+			y[i] = i;
+			System.out.println("y["+i+"]:"+y[i]);
+		}
+		passagemDeArray(y);
+		for(int i =0; i < 5; i++){
+			System.out.println("y["+i+"]:"+y[i]);
+		}
+	}
+}
