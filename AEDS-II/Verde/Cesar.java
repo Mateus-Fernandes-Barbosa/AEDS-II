2ba43bf137bebc587ae6d6b94afeb2d01e9848bb
Mateus Fernandes Barbosa <mateusfb04@gmail.com>
2023-08-30 11:21:49 -0300

    Atualização do criptógrafo de Cesar

--- AEDS-II/Verde/Cesar.java
+++ AEDS-II/Verde/Cesar.java
@@ -10,7 +10,15 @@ class Cesar{
 		int n = palavra.length();
 		int [] temp = new int [n];
 		for(int i = 0; i < n; i++){
-			temp[i] = (int) palavra.charAt(i) + 3;
+			if(palavra.charAt(i) != ' '){	
+				temp[i] = (int) palavra.charAt(i) + 9;
+				if((int) palavra.charAt(i) + 9 > 117){
+					temp[i] = (((int) palavra.charAt(i) + 9 + 137) - 255) + 33;
+				}
+			}
+			else{
+				temp[i] = palavra.charAt(i);
+			}
 		}
 		palavra = "";
 		for(int i = 0; i < n; i++){
@@ -18,15 +26,81 @@ class Cesar{
 		}
 		return palavra;
 	}
-	public static void main (String[] args){
-//		MyIO.setCharset("UTF-8");
-		String palavra, encriptada;
-		palavra = MyIO.readLine();
-		while(!Strcmp(palavra)){
-			encriptada = CriptografiaCesar(palavra);
-			MyIO.println(encriptada);
-			palavra = MyIO.readLine();
-		}	
+	public static String DescriptografiaCesar(String palavra, int key){
+		int n = palavra.length();
+                int [] temp = new int [n];
+                for(int i = 0; i < n; i++){
+			if(palavra.charAt(i) != ' '){
+                        	temp[i] = (int) palavra.charAt(i) - key;
+				if((int) palavra.charAt(i) - key < 33){
+					temp[i] = (((int) palavra.charAt(i) - 33) + 255) - 137 - 9;
+				}
+			}
+			else{
+				temp[i] = palavra.charAt(i);
+			}
+                }
+                palavra = "";
+                for(int i = 0; i < n; i++){
+                        palavra += (char)temp[i];
+                }
+                return palavra;
+	}
+	public static int menu(){
+		int opcao;
+		MyIO.println("Escolha uma das opções abaixo:"+
+			     "\n[0] Sair"+
+			     "\n[1] Criptografar"+
+			     "\n[2] Descriptografar");
+		opcao = MyIO.readInt();
+		return opcao;
+	}
+	public final static void clearConsole() //Método pego da internet
+{
+    try
+    {
+        final String os = System.getProperty("os.name");
 
+        if (os.contains("Windows"))
+        {
+            Runtime.getRuntime().exec("cls");
+        }
+        else
+        {
+            Runtime.getRuntime().exec("clear");
+        }
+    }
+    catch (final Exception e)
+    {
+        //  Handle any exceptions.
+    }
+}
+	public static void main (String[] args){
+		MyIO.setCharset("UTF-8");
+		String palavra, resultado;
+		int opcao, key;
+		do{
+			clearConsole();
+			opcao = menu();
+			switch(opcao){
+				case 0:
+					MyIO.println("Flw!");
+					break;
+				case 1:
+					MyIO.println("Escreva a mensagem:");
+					palavra = MyIO.readLine();
+					resultado = CriptografiaCesar(palavra);
+					MyIO.println("Resultado: "+resultado);
+					break;
+				case 2:
+					MyIO.println("Escreva a mensagem:");
+					palavra = MyIO.readLine();
+					MyIO.println("Escreva a chave:");
+					key = MyIO.readInt();
+					resultado = DescriptografiaCesar(palavra, key);
+					MyIO.println("Resultado: "+resultado);
+					break;
+			}
+		}while(opcao != 0);	
 	}
 }
